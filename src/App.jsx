a55d6fc3965143cc7951c368eeb73e0bee91f9ec
frievoe97 <friedrich.voelkers@me.com>
2023-10-22 16:04:31 +0200

    better design for emergency button

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import ScrollToTop from "./components/utils/ScrollToTop";
 import Machtmissbrauch from "./components/Machtmissbrauch";
 import WarumJetzt from "./components/WarumJetzt";
 import Examples from "./components/Examples";
+import CookieBanner from "./components/CookieBanner";
 
 function App() {
   const [acceptCookies, setAcceptCookies] = useState(false);
@@ -46,24 +47,7 @@ function App() {
           </Routes>
         </GlobalStateProvider>{" "}
       </Layout>
-      {acceptCookies && (
-        <div className="flex justify-between items-center gap-2 bg-gray-100 px-6 py-4 fixed bottom-0 left-0 w-full">
-          <p className="text-sm text-gray-700">
-            Wir verwenden Cookies, um Ihnen das beste Erlebnis auf unserer
-            Website zu bieten. Lerne&nbsp;
-            <a href="#" className="underline">
-              hier
-            </a>
-            &nbsp;mehr.
-          </p>
-          <button
-            onClick={handleAcceptCookies}
-            className="text-black bg-green-500 hover:bg-green-600 font-bold py-2 px-4 border-2 border-black rounded"
-          >
-            Accept
-          </button>
-        </div>
-      )}
+      <CookieBanner />
     </Router>
   );
 }
--- src/components/EmergencyButton.jsx
+++ src/components/EmergencyButton.jsx
@@ -13,10 +13,30 @@ const EmergencyButton = () => {
     setShowEmergencyDialog(false);
   };
 
+  // Definiere eine Liste von Notrufnummern und deren Beschreibungen
+  const emergencyNumbers = [
+    {
+      title: "Polizei",
+      number: "110",
+      description: "Bei kriminellen Vorfällen und Sicherheitsbedenken.",
+    },
+    {
+      title: "Feuerwehr",
+      number: "112",
+      description: "Bei Feuer- und Brandsituationen sowie anderen Notfällen.",
+    },
+    {
+      title: "Ärztlicher Notdienst",
+      number: "116 117",
+      description:
+        "Bei gesundheitlichen Notfällen außerhalb der regulären Praxiszeiten.",
+    },
+  ];
+
   return (
     <div className="fixed right-0 bottom-24 p-4 pr-0">
       <button
-        className="bg-gray-100 text-black p-4 text-lg shadow-md hover:bg-red-600 rounded-none rounded-l-lg"
+        className="bg-gray-100 text-black p-4 text-lg shadow-md rounded-none rounded-l-lg"
         onClick={openEmergencyDialog}
       >
         <FontAwesomeIcon icon={faPhone} />
@@ -25,38 +45,46 @@ const EmergencyButton = () => {
       {showEmergencyDialog && (
         <div className="fixed inset-0 flex justify-center items-center bg-gray-800 bg-opacity-75">
           <div className="bg-white rounded-lg p-8 max-w-screen-md shadow-md">
-            <div className="flex flex-row">
-              <h2 className="text-3xl font-semibold mb-4 text-center text-gray-800">
-                Notrufnummern
-              </h2>
+            <div className="flex flex-row items-center mb-4">
               <button
-                className="text-gray-600 hover:text-red-500"
+                className="bg-color_4 text-gray-600 mr-4"
                 onClick={closeEmergencyDialog}
               >
                 <FontAwesomeIcon icon={faTimes} size="lg" />
               </button>
+              <h2 className="text-3xl font-semibold text-center text-gray-800">
+                Sofortige Hilfe benötigt?
+              </h2>
             </div>
+            <p className="text-lg text-gray-700 mb-4">
+              In dringenden Notfällen stehen Ihnen die folgenden Notrufnummern
+              zur Verfügung:
+            </p>
             <ul className="space-y-4 text-lg">
-              <li className="flex items-center">
-                <span className="w-fit text-gray-600 font-semibold">
-                  Polizei:
-                </span>
-                &nbsp; 110
-              </li>
-              <li className="flex items-center">
-                <span className="w-fit text-gray-600 font-semibold">
-                  Feuerwehr:
-                </span>
-                &nbsp; 112
-              </li>
-              <li className="flex items-center">
-                <span className="w-fit text-gray-600 font-semibold">
-                  Ärztlicher Notdienst:
-                </span>
-                &nbsp; 116 117
-              </li>
-              {/* Fügen Sie weitere Notrufnummern hinzu */}
+              {emergencyNumbers.map((emergency, index) => (
+                <li key={index} className="flex flex-col items-center w-full">
+                  <div className="flex flex-row w-full">
+                    <span
+                      className="text-gray-600 font-semibold"
+                      style={{ whiteSpace: "nowrap" }}
+                    >
+                      {emergency.title}:
+                    </span>
+                    &nbsp;
+                    <a href={`tel:${emergency.number + 123}`}>
+                      {emergency.number}
+                    </a>
+                  </div>
+                  <span className="text-gray-500 w-full">
+                    {emergency.description}
+                  </span>
+                </li>
+              ))}
             </ul>
+            <p className="mt-4 text-gray-700">
+              Bitte rufen Sie die entsprechende Nummer an, wenn Sie sich in
+              einer Notsituation befinden.
+            </p>
           </div>
         </div>
       )}
