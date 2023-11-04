d2f493d8aaafdc92841be3f543a77da8e3f4d6bc
frievoe97 <friedrich.voelkers@me.com>
2023-11-04 12:13:31 +0100

    update anlaufstellen

--- src/scripts/parse_csv_to_yaml.py
+++ src/scripts/parse_csv_to_yaml.py
@@ -1,48 +1,31 @@
 import csv
 import yaml
 
-# Name der CSV-Datei
+# Pfad zur CSV-Datei
 csv_file = 'data.csv'
 
-# Name der YAML-Datei
+# Pfad zur YAML-Datei
 yaml_file = 'data.yaml'
 
-# Öffne die CSV-Datei zum Lesen
-with open(csv_file, 'r', newline='', encoding='utf-8') as csv_file:
-    # Lese die CSV-Daten und entferne Zeilenumbrüche und leere Einträge
-    csv_reader = csv.reader(csv_file)
-    data = [list(map(lambda x: x.strip().replace('\n', ''), row)) for row in csv_reader if any(row)]
+# Lesen der CSV-Datei und Konvertieren in ein Python-Dictionary
+data = []
+current_category = None
 
-# Initialisiere die Kategorie-Variable
-category = None
+with open(csv_file, newline='', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    header = next(reader)  # Die erste Zeile enthält die Spaltennamen
+    for row in reader:
+        if row[0]:  # Wenn die Kategorie-Zeile nicht leer ist
+            current_category = {"Kategorie": row[0], "Name": []}
+        elif current_category is not None and any(row[1:]):  # Wenn es Tags gibt
+            tag = {"Name": row[1], "Tags": [header[col] for col, value in enumerate(row[2:]) if value == "x"]}
+            current_category["Name"].append(tag)
+        elif current_category is not None:  # Wenn die Tags abgeschlossen sind
+            data.append(current_category)
+            current_category = None
 
-# Erstelle ein Dictionary aus den bereinigten CSV-Daten
-result = {}
-for row in data:
-    if row[0]:
-        category = row[0]
-        print(category)
-        result[category] = []
-    elif category is not None and row[1]:
-        kategorie_machtmissbrauch = [item.strip() for item in row[4].split('/')]
-        entry = {
-            'Vorname': row[1],
-            'Nachname': row[2],
-            'Branche': row[3],
-            'Kategorie Machtmissbrauch': kategorie_machtmissbrauch,
-            'Social Media-Präsenz': row[5],
-            'Organisation / Projekt': row[6],
-            'Publikationen': row[7],
-            'Contacted': row[8],
-            'Status': row[9]
-        }
-        # Filtere leere Werte aus dem Eintrag
-        entry = {k: v for k, v in entry.items() if v}
-        if entry:
-            result[category].append(entry)
+# Konvertieren des Python-Liste in YAML
+with open(yaml_file, 'w', encoding='utf-8') as yamlfile:
+    yaml.dump(data, yamlfile, default_flow_style=False, allow_unicode=True)
 
-# Speichere das Dictionary in einer YAML-Datei
-with open(yaml_file, 'w', encoding='utf-8') as yaml_file:
-    yaml.dump(result, yaml_file, allow_unicode=True)
-
-print(f'Die CSV-Datei wurde in {yaml_file} konvertiert und gespeichert.')
+print(f'Die Daten wurden erfolgreich als {yaml_file} gespeichert.')
