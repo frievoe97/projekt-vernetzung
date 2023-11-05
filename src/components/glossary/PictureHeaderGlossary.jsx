442c1eb3bb9430bd22dd4f8e90c1c354ad49a36a
frievoe97 <friedrich.voelkers@me.com>
2023-11-05 01:29:44 +0100

    update .yaml

--- src/components/glossary/PictureHeaderGlossary.jsx
+++ src/components/glossary/PictureHeaderGlossary.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
-const PictureHeaderGlossary = () => {
-  const imageUrl =
-    "https://github.com/frievoe97/projekt-vernetzung/blob/main/src/assets/iamges/Image-2.jpeg?raw=true";
+const PictureHeaderGlossary = ({ data }) => {
+  const { imageUrl, title, subtitle, descriptions } = data;
 
   return (
     <div
@@ -10,37 +9,13 @@ const PictureHeaderGlossary = () => {
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
       <div className="max-w-screen-xl mx-auto bg-white p-4 text-justify md:text-center rounded">
-        <h1 className="text-2xl font-bold text-fm_blau mb-4">Glossar</h1>
-        <h2 className="text-lg font-semibold">
-          Hier möchten wir dir die wichtigsten Begriffe im Kontext von
-          Machtmissbrauch erläutern
-        </h2>
-        <p className="mt-4">
-          Betroffene sollen auf dieser Seite schneller und einfacher
-          Informationen finden, die auf ihre persönliche Situation zutreffen
-          könnten. Davon ausgehend, können sie sich weiter informieren und
-          insbesondere das in Worte fassen, was oftmals schwer in Worte zu
-          fassen ist.
-        </p>
-        <p className="mt-4">
-          Wir erheben keinen Anspruch auf Vollständigkeit. Unsere Auswahl an
-          Begrifflichkeiten wird kontinuierlich auf den Prüfstand gestellt. Für
-          konstruktive Anregungen und Ideen sind wir jederzeit dankbar.
-        </p>
-        {/* <ul className="list-disc ml-6 mt-2">
-          <li className="text-left w-fit mx-auto">
-            das Problem in seiner Drastik in allen Lebensbereichen und Branchen
-            unserer Gesellschaft darstellen,
-          </li>
-          <li className="text-left w-fit mx-auto">
-            möglichst präventiv und umfassend informieren, sensibilisieren und
-            Handlungsoptionen aufzeigen,
-          </li>
-          <li className="text-left w-fit mx-auto">
-            gemeinsam mit Dritten erörtern, was sich ändern muss und was Jede:r
-            einzelne beitragen kann.
-          </li>
-        </ul> */}
+        <h1 className="text-2xl font-bold text-fm_blau mb-4">{title}</h1>
+        <h2 className="text-lg font-semibold">{subtitle}</h2>
+        {descriptions.map((description, index) => (
+          <p key={index} className="mt-4">
+            {description}
+          </p>
+        ))}
       </div>
     </div>
   );
