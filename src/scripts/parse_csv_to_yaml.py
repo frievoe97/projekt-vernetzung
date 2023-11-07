cfde515cdc367a8897e8b72cdaca889a0b04966f
frievoe97 <friedrich.voelkers@me.com>
2023-11-07 17:29:02 +0100

    Card design mobile friendly, better glossary header

--- src/scripts/parse_csv_to_yaml.py
+++ src/scripts/parse_csv_to_yaml.py
@@ -16,10 +16,11 @@ with open(csv_file, newline='', encoding='utf-8') as csvfile:
     header = next(reader)  # Die erste Zeile enthält die Spaltennamen
     for row in reader:
         if row[0]:  # Wenn die Kategorie-Zeile nicht leer ist
-            current_category = {"Kategorie": row[0], "Name": []}
+            current_category = {"Kategorie": row[0], "Anlaufstelle": []}
         elif current_category is not None and any(row[1:]):  # Wenn es Tags gibt
-            tag = {"Name": row[1], "Tags": [header[col] for col, value in enumerate(row[2:]) if value == "x"]}
-            current_category["Name"].append(tag)
+            tag = {"Name": row[1], "Link": row[20], "Tags": [header[col] for col, value in enumerate(row[2:]) if value == "x"]}
+            current_category["Anlaufstelle"].append(tag)
+            # current_category["Link"].append(row[20])
         elif current_category is not None:  # Wenn die Tags abgeschlossen sind
             data.append(current_category)
             current_category = None
