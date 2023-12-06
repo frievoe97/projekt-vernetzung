7cb544f6fc25c268ed5f8b8e7a8dd6bf4db48be4
frievoe97 <friedrich.voelkers@me.com>
2023-12-06 20:53:24 +0100

    update anlaufstellen (subheader, disclaimer, yaml content)

--- src/components/anlaufstellen/SearchBarAnt.jsx
+++ src/components/anlaufstellen/SearchBarAnt.jsx
@@ -35,7 +35,17 @@ function SearchBarAnt({
   return (
     <div className="w-full mb-4 bg-fm_weiss">
       <div className="max-w-screen-xl mx-auto">
-        <h2 className="heading py-12">Anlaufstellen</h2>
+        <h2 className="heading pt-12 mb-6">Anlaufstellen</h2>
+        <p className="font-bold text-lg mb-4">
+          Potenziell relevante Anlaufstellen im Kontext von Machtmissbrauch{" "}
+        </p>
+        <p className="max-w-screen-lg mx-auto pb-10 text-base">
+          <b>Disclaimer:</b> Projekt Vernetzung erhebt keinen Anspruch auf
+          Vollständigkeit und stellt die eigene Auswahl an Anlaufstellen
+          fortlaufend auf den Prüfstand. Falls wir aus deiner Sicht etwas
+          vergessen haben, sind wir für konstruktive Anregungen und Ideen
+          jederzeit dankbar.
+        </p>
         {/* <p className="pb-12 text-lg">
           Suche nach deiner passenden Anlaufstelle
         </p> */}
