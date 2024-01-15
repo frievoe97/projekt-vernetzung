c002157b144f2d9d114d55e5d5259533f998d3de
frievoe97 <friedrich.voelkers@me.com>
2024-01-15 17:11:17 +0100

    update imprint

--- src/components/imprint/DataPrivacy.jsx
+++ src/components/imprint/DataPrivacy.jsx
@@ -6,43 +6,12 @@ const DataPrivacy = () => {
       <div className="p-0 md:p-6 text-center z-0 text-color_font">
         <div className="grid text-center gap-0 mx-auto px-0 md:px-6 lg:px-8 max-w-screen-xl my-0">
           <div className="md:bg-fm_weiss p-4 rounded-xl my-16 w-fit mx-auto p-8 md:shadow-2xl">
-            <h1 className="text-2xl font-bold text-fm_blau mb-4">
-              Datenschutzerklärung
+            <h1 className="text-2xl font-bold text-fm_blau mb-4 heading-koulen">
+              Datenschutzerklaerung
             </h1>
             <div className="text-left w-fit mx-auto">
               <h2 className="text-xl font-bold mt-4 w-fit">
-                1. Speicherung personenbezogener Daten
-              </h2>
-              <p className="mt-2 w-fit text-justify">
-                Im rechtlichen Sinne beziehen sich personenbezogene Daten auf
-                Informationen, die sich auf eine identifizierte oder
-                identifizierbare natürliche Person beziehen. Das schließt Daten
-                ein, die direkt oder indirekt verwendet werden können, um die
-                Identität einer Person festzustellen, wie beispielsweise Namen,
-                Adressen, E-Mail-Adressen, Telefonnummern oder andere
-                persönliche Merkmale. In Übereinstimmung mit den rechtlichen
-                Bestimmungen und zum Schutz der Privatsphäre unserer Nutzer
-                möchten wir klarstellen, dass auf dieser Website keine
-                personenbezogenen Daten im Sinne dieser Definition gespeichert
-                oder verarbeitet werden.
-              </p>
-
-              <h2 className="text-xl font-bold mt-4 w-fit">2. Cookies</h2>
-              <p className="mt-2 w-fit text-justify">
-                Cookies sind kleine Textdateien, die von einer Website auf
-                deinem Computer oder Gerät gespeichert werden. Sie dienen dazu,
-                Informationen über deine Interaktionen mit der Website zu
-                speichern und ermöglichen es der Website, dich bei zukünftigen
-                Besuchen wiederzuerkennen. Wir möchten darauf hinweisen, dass
-                auf dieser Website keine Cookies verwendet werden. Das bedeutet,
-                dass keine dieser kleinen Dateien auf deinem Gerät gespeichert
-                werden, um Informationen über deine Nutzung der Website zu
-                sammeln oder dich bei späteren Besuchen wiederzuerkennen. Deine
-                Privatsphäre und deine Daten bleiben geschützt.
-              </p>
-
-              <h2 className="text-xl font-bold mt-4 w-fit">
-                3. Verwendung von Analysediensten
+                1. Verwendung von Analysediensten
               </h2>
               <p className="mt-2 w-fit text-justify">
                 Unsere Webhosting-Infrastruktur erfasst bestimmte Daten in
@@ -55,6 +24,119 @@ const DataPrivacy = () => {
                 123.123.123.123 durch eine zufällige IP-Adresse 123.123.123.XXX
                 ersetzt wird, wobei XXX ein Wert zwischen 1 und 254 ist, sodass
                 keine Herstellung eines Personenbezugs mehr möglich ist.
+              </p>
+
+              <h2 className="text-xl font-bold mt-4 w-fit">2. Newsletter</h2>
+              <p className="mt-2 w-fit text-justify">
+                Wenn du den auf der Website angebotenen Newsletter beziehen
+                möchtest, benötigen wir von dir eine Email-Adresse sowie
+                Informationen, welche uns die Überprüfung gestatten, dass du der
+                Inhaber der angegebenen E-Mail-Adresse und mit dem Empfang des
+                Newsletters einverstanden bist. Weitere Daten werden nicht bzw.
+                nur auf freiwilliger Basis erhoben. Diese Daten verwenden wir
+                ausschließlich für den Versand der angeforderten Informationen
+                und geben diese nicht an Dritte weiter. Wir lassen dir unseren
+                Newsletter auf Grundlage deiner Einwilligung zukommen (Art. 6
+                Abs. 1 S. 1 a DSGVO). Du hast jederzeit die Möglichkeit, dich
+                vom Newsletter wieder abzumelden bzw. deine Einwilligung mit
+                Wirkung für die Zukunft zu widerrufen. Schreibe hierzu einfach
+                an unsere E-Mail-Adresse{" "}
+                <a
+                  href="mailto:projekt-vernetzung@email.com"
+                  className="mt-2 w-fit  font-normal"
+                >
+                  info@projekt-vernetzung.org
+                </a>
+                . Die von dir zum Zwecke des Newsletterbezugs bei uns
+                hinterlegten Daten werden von uns bis zu deiner Austragung aus
+                dem Newsletter bei uns bzw. dem Newsletterdiensteanbieter
+                gespeichert und nach der Abbestellung des Newsletters oder nach
+                Zweckfortfall aus der Newsletterverteilerliste gelöscht. Sobald
+                wir uns für einen hinreichend zum Datenschutz verpflichteten
+                Newsletterdienst entschieden haben, informieren wir dich hier
+                zudem über den Anbieter.
+              </p>
+
+              <h2 className="text-xl font-bold mt-4 w-fit">5. Google Fonts</h2>
+              <p className="mt-2 w-fit text-justify">
+                3. Google Fonts Diese Seite nutzt zur einheitlichen Darstellung
+                von Schriftarten sogenannte Google Fonts, die von Google
+                bereitgestellt werden. Die Google Fonts sind lokal installiert.
+                Eine Verbindung zu Servern von Google findet dabei nicht statt.
+                Weitere Informationen zu Google Fonts findest du unter{" "}
+                <a href="https://developers.google.com/fonts/faq">
+                  https://developers.google.com/fonts/faq
+                </a>
+                und in der Datenschutzerklärung von Google:{" "}
+                <a href="https://policies.google.com/privacy?hl=de">
+                  https://policies.google.com/privacy?hl=de
+                </a>
+                .
+              </p>
+
+              <h2 className="text-xl font-bold mt-4 w-fit">
+                4. Datenverarbeitung auf Instagram
+              </h2>
+              <p className="mt-2 w-fit text-justify">
+                Wir haben eine Social-Media-Präsenz auf Instagram. Wenn du ein
+                Profil auf einer Social-Media-Plattform besuchst oder mit diesem
+                interagierst, kann es zu einer Verarbeitung von
+                personenbezogenen Daten über dich kommen. Auch die mit einem
+                verwendeten Social-Media-Profil verbundenen Informationen
+                stellen regelmäßig personenbezogene Daten dar. Dies umfasst auch
+                Nachrichten und Aussagen, die unter der Verwendung des Profils
+                gemacht werden. Außerdem werden während deines Besuchs eines
+                Social-Media-Profils oftmals automatisch bestimmte Informationen
+                hierüber erfasst, die auch personenbezogene Daten darstellen
+                können. Allein Verantwortlicher für diese Verarbeitung
+                personenbezogener Daten ist die Meta Platforms Ireland Ltd.
+                (Irland/EU – „Meta“). Weitere Hinweise über die Verarbeitung
+                personenbezogener Daten durch Meta erhältst du unter{" "}
+                <a href="https://www.facebook.com/privacy/explanation">
+                  https://www.facebook.com/privacy/explanation
+                </a>
+                . Meta bietet die Möglichkeit, bestimmten Datenverarbeitungen zu
+                widersprechen; diesbezügliche Hinweise und Opt-Out-Möglichkeiten
+                finden sich unter{" "}
+                <a href="https://www.facebook.com/settings?tab=ads">
+                  https://www.facebook.com/settings?tab=ads
+                </a>
+                . Meta stellt uns für unsere Instagram-Seite in anonymisierter
+                Form Statistiken und Einblicke bereit, mit deren Hilfe wir
+                Erkenntnisse über die Arten von Handlungen erhalten, die
+                Personen auf unserer Seite vornehmen (sog. „Insights“). Diese
+                Insights werden auf der Grundlage von bestimmten Informationen
+                über Personen, die unsere Seite besucht haben, erstellt. Diese
+                Verarbeitung personenbezogener Daten erfolgt durch Meta und uns
+                als gemeinsam Verantwortliche. Die Verarbeitung dient unserem
+                berechtigten Interesse, die Arten von vorgenommenen Handlungen
+                auf unserer Seite auszuwerten und unsere Seite anhand dieser
+                Erkenntnisse zu verbessern. Rechtsgrundlage für diese
+                Verarbeitung ist Art. 6 Abs. 1 lit. f DSGVO. Wir können die über
+                die Insights erhaltenen Informationen nicht einzelnen
+                Nutzer:innen-Profilen, die mit unserer Instagram-Seite
+                interagieren, zuordnen. In Bezug auf diese Datenverarbeitungen
+                hast du die Möglichkeit, deine Betroffenenrechte (siehe hierzu
+                „deine Rechte”) auch gegenüber Meta geltend zu machen. Weitere
+                Informationen hierzu enthält die Datenschutzerklärung von Meta
+                unter{" "}
+                <a href="https://www.facebook.com/privacy/explanation">
+                  https://www.facebook.com/privacy/explanation
+                </a>
+                . Bitte beachte, dass gemäß den Meta-Datenschutzbestimmungen
+                Nutzer:innendaten auch in den USA oder anderen Drittländern
+                verarbeitet werden. Meta überträgt Nutzer:innendaten nur in
+                Länder, für die ein Angemessenheitsbeschluss der Europäischen
+                Kommission nach Art. 45 DSGVO oder auf Grundlage geeigneter
+                Garantien nach Art. 46 DSGVO vorliegt. Wir verarbeiten außerdem
+                Informationen, die du uns über unsere
+                Instagram-Unternehmensseite zur Verfügung gestellt hast. Solche
+                Informationen können der genutzte Username, Kontaktdaten oder
+                eine Mitteilung an uns sein. Diese Verarbeitungen durch uns
+                erfolgen als allein Verantwortlicher. Wir verarbeiten diese
+                Daten aufgrund unseres berechtigten Interesses, mit anfragenden
+                Personen in Kontakt zu treten. Rechtsgrundlage für die
+                Datenverarbeitung ist Art. 6 Abs. 1 lit. f DSGVO.
               </p>
             </div>
           </div>
--- src/components/imprint/Imprint.jsx
+++ src/components/imprint/Imprint.jsx
@@ -5,32 +5,79 @@ const Imprint = () => {
     <div className="pt-16">
       <div className="p-0 md:p-6 text-center z-0 text-color_font">
         <div className="grid text-center gap-0 mx-auto px-0 md:px-6 lg:px-8 max-w-screen-xl my-0">
-          <div className="md:bg-fm_weiss p-4 rounded-xl mt-16 w-fit mx-auto p-8 md:shadow-2xl">
-            <h1 className="text-2xl font-bold text-fm_blau mb-4">Impressum</h1>
+          <div className="md:bg-fm_weiss p-4 rounded-xl my-16 w-fit mx-auto p-8 md:shadow-2xl">
+            <h1 className="text-2xl font-bold text-fm_blau mb-4 heading-koulen">
+              Impressum
+            </h1>
             <div className="text-left w-fit mx-auto">
               <h2 className="text-xl font-bold mt-4 w-fit">
-                Verantwortlich für den Inhalt dieser Website:
+                Angaben gemäß §5 TMG:
               </h2>
-              <p className="mt-2 w-fit">Projekt Vernetzung</p>
-              <p className="w-fit">Berlin, Deutschland</p>
-
-              <h2 className="text-xl font-bold mt-4 w-fit">Kontakt:</h2>
-              {/* <p className="mt-2 w-fit">E-Mail: info@projekt-vernetzung.org</p> */}
-
-              <a
-                href="mailto:projekt-vernetzung@email.com"
-                className="mt-2 w-fit text-black hover:text-black font-normal"
-              >
-                E-Mail: info@projekt-vernetzung.org
-              </a>
-
-              <h2 className="text-xl font-bold mt-4 w-fit">Haftungshinweis:</h2>
               <p className="mt-2 w-fit">
-                Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine
-                Haftung für die Inhalte externer Links.
+                Projekt Vernetzung ist eine private Initiative, vertreten durch
+                Hannah Adam.
+              </p>
+              <h2 className="text-xl font-bold mt-4 w-fit">Kontakt:</h2>
+              <p className="mt-2">
+                <a
+                  href="mailto:projekt-vernetzung@email.com"
+                  className="mt-2 w-fit text-black hover:text-black font-normal"
+                >
+                  E-Mail: info@projekt-vernetzung.org
+                </a>
+              </p>
+              <p>
+                <a
+                  className="mt-2 w-fit text-black hover:text-black font-normal"
+                  href="https://projekt-vernetzung.org"
+                  target="_blank"
+                >
+                  Website: www.projekt-vernetzung.org
+                </a>
+              </p>
+              <p className="w-fit">Adresse: Berlin</p>
+              <p className="mt-2 w-fit">
+                ‍Verantwortliche i.S.d. § 18 Abs. 2 MStV ist Hannah Adam.
               </p>
 
-              <p className="mt-8 w-fit">© 2023 Projekt Vernetzung</p>
+              <h2 className="text-xl font-bold mt-4 w-fit">
+                Haftung für Inhalte
+              </h2>
+              <p className="mt-2 w-fit">
+                Als Diensteanbieter ist Projekt Vernetzung gemäß § 7 Abs. 1 TMG
+                für eigene Inhalte auf diesen Seiten nach den allgemeinen
+                Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir jedoch
+                nicht verpflichtet, übermittelte oder gespeicherte fremde
+                Informationen zu überwachen oder nach Umständen zu forschen, die
+                auf eine rechtswidrige Tätigkeit hinweisen.
+              </p>
+              <p className="mt-2 w-fit">
+                Bei Bekanntwerden von entsprechenden Rechtsverletzungen wird
+                Projekt Vernetzung entsprechende Inhalte umgehend entfernen.
+              </p>
+              <h2 className="text-xl font-bold mt-4 w-fit">
+                Haftung für Links
+              </h2>
+              <p className="mt-2 w-fit">
+                Das Angebot von Projekt Vernetzung enthält Links zu externen
+                Websites Dritter, auf deren Inhalte wir keinen Einfluss haben
+                und auch keine Gewähr dafür übernehmen können. Für die Inhalte
+                der verlinkten Seiten ist stets der jeweilige Anbieter oder
+                Betreiber der Seiten verantwortlich. Die verlinkten Seiten
+                wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße
+                überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der
+                Verlinkung nicht erkennbar.
+              </p>
+              <h2 className="text-xl font-bold mt-4 w-fit">Urheberrecht</h2>
+              <p className="mt-2 w-fit">
+                Die durch die Seitenbetreiber erstellten Inhalte und Werke auf
+                diesen Seiten unterliegen dem deutschen Urheberrecht. Die
+                Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+                Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der
+                schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+                Downloads und Kopien dieser Seite sind nur für den privaten,
+                nicht kommerziellen Gebrauch gestattet.
+              </p>
             </div>
           </div>
         </div>
