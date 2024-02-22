bb1e757a99066e14f692a9f3b9583d15bb2c4339
frievoe97 <friedrich.voelkers@me.com>
2024-02-22 22:56:12 +0100

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import DataPrivacy from "./components/imprint/DataPrivacy";
 import Interviews from "./components/interviews/Interviews";
 import InterviewDetail from "./components/interviews/InterviewDetail";
 
+// App
 function App() {
   return (
     <GlobalStateProvider>
